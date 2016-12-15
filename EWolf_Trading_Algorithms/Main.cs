39c013257c6f1504f910a552a979c466240421d4
Кирилл <caesarkf@gmail.com>
2016-12-15 20:09:05 +0300

    Starting Main

--- EWolf_Trading_Algorithms/Main.cs
+++ EWolf_Trading_Algorithms/Main.cs
@@ -9,38 +9,16 @@ namespace EWolf_Trading_Algorithms
 {
 	class Main
 	{
-		public List<Position> Positions;
-		public Dictionary<string, List<Deal>> Deals;
-		Data D;
-
-		void Open_Deal_1()
-		{
-		}
-
-		void Close_Deal_1()
-		{
-		}
-
-		public void Tick()
-		{
-			for (int i = 0; i < D.Companies.Count; i++)
-			{
-				for (int j = 0; j < D.Companies[i].Time_Frames.Count; j++)
-					D.Companies[i].Update(D.Companies[i].Ticker, D.Companies[i].Time_Frames[j]);
-			}
-			Open_Deal_1();
-			Close_Deal_1();
-		}
+		private Dictionary<string, Stock> Stocks;
+		private Data D;
 
 		public Main()
 		{
-			Positions = new List<Position> { };
-			Deals = new Dictionary<string, List<Deal>> { };
+			Stocks = new Dictionary<string, Stock> { };
 			D = new Data();
 			for (int i = 0; i < D.Tickers.Count; i++)
 			{
-				Positions.Add(new Position(D.Tickers[i], 0));
-				Deals[D.Tickers[i]] = new List<Deal> { };
+				Stocks[D.Tickers[i]] = new Stock(D.Tickers[i]);
 			}
 		}
 	}
