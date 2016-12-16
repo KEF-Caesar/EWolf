f9b49d617de1c8e98a2d3045c325329f2c06945c
Катя <@VAIO>
2016-12-16 14:19:24 +0300

    Imlementing method GetCandles for downloading candles data and saving it to file and adding example of its usage.

--- EWolf_Input/Repository_For_Candles.cs
+++ EWolf_Input/Repository_For_Candles.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
+using System.Net;
+using System.IO;
 
 namespace EWolf_Input
 {
-    class Repository_For_Candles
+   public class Repository_For_Candles
     {
         const string source = "http://export.finam.ru/1.txt?market=1&em=3&code={0}&apply=0&df={1}&mf={2}&yf={3}&from={4}&dt={5}&mt={6}&yt={7}&to={8}&p={9}&f=1&e=.txt&cn={0}&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1";
         private List<string> periods = new List<string> { "M1", "M5", "M10", "M15", "M30", "H1", "D1" };
@@ -18,6 +17,35 @@ namespace EWolf_Input
         {
             _Format = format;
             _Period = period;
+        }
+        public async void GetCandles(string ticker, string period, int days)
+        {
+            using (WebClient client = new WebClient())
+            {
+                DateTime todate = DateTime.Now;
+                DateTime fromdate = todate.AddDays(-days);
+                string[] dateformatfrom = { fromdate.Year.ToString(), (fromdate.Month - 1).ToString(), fromdate.Day.ToString() };
+                string dateformatfr = fromdate.Date.ToString();
+                string[] dateformatto = { todate.Year.ToString(), (todate.Month - 1).ToString(), todate.Day.ToString() };
+                string dateformatt = todate.Date.ToString();
+                int periodformat = periods.FindIndex(item => item == period) + 2;
+                string currentsource = string.Format(source, ticker, dateformatfrom[2], dateformatfrom[1], dateformatfrom[0], dateformatfr, dateformatto[2], dateformatto[1], dateformatto[0], dateformatt, periodformat);
+                Uri uri = new Uri(currentsource);
+                client.DownloadStringAsync(uri);
+                client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadStringCallback);
+
+            }
+        }
+        public void DownloadStringCallback(object s, DownloadStringCompletedEventArgs e)
+        {
+            string str = e.Result;
+            FileStream file = new FileStream("..\\..\\..\\EWolf_Data\\Data\\" + Format + " " + Period + ".txt", FileMode.Create);
+            using (StreamWriter sw = new StreamWriter(file, System.Text.Encoding.Default))
+            {
+                sw.Write(str);
+            }
+            file.Close();
+
         }
     }
 }
--- EWolf_Test/Program.cs
+++ EWolf_Test/Program.cs
@@ -4,14 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EWolf_Input;
 namespace EWolf_Test
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Success!");
+            // Пример использования метода для получения свечей и их записи в файл
+            Repository_For_Candles repo;
+            repo = new Repository_For_Candles("SBER", "M1");
+            repo.GetCandles("SBER", "M1", 5);
+            Console.WriteLine("Success!");
 			Console.ReadLine();
 		}
 	}
